6a3239fc072e727c412e6201b51c612730188335
MercyAngelin <mercyangelin2000@gmail.com>
2022-09-26 16:38:14 +0530

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const path=require("path");
 const app=express();
-app.use(express.static(__dirname+'/dist/school-project'));
+app.use(express.static(__dirname+'/dist/edustar'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist'+'/school-project/index.html'));
+    res.sendFile(path.join(__dirname+'/dist'+'/edustar/index.html'));
 
 });
 
